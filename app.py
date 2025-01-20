7077a61731a81f03c8dba754633ab246075a038b
noegruoc <36902054+noegruoc@users.noreply.github.com>
2025-01-20 23:44:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ if submit_button_pressed:
 if st.session_state["generated"]:
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=f"{i}_user", avatar_style="bottts", seed=Jude)
+        message(st.session_state["past"][i], is_user=True, key=f"{i}_user", avatar_style="bottts", seed=129)
